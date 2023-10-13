15d4c6f24c78005f2dde88a33c41a6d89e0d2021
Ridoy Rossain <ridoy.hossain@bracits.com>
2023-10-13 09:36:13 +0600

    Callback function

--- callback.js
+++ callback.js
@@ -1,8 +1,19 @@
-function hello(name) {
+// function hello(name) {
+//   print(name);
+//   print("somethings else");
+// }
+// function print(args) {
+//   console.log(args);
+// }
+// hello("Hridoy Hossain");
+
+function hello(name, print) {
   print(name);
-  print("somethings else");
 }
-function print(args) {
-  console.log(args);
-}
-hello("Hridoy Hossain");
+hello("hridoy", function (name) {
+  console.log("Hello " + name);
+});
+
+hello("hossain", function (name) {
+  console.log("how are you " + name);
+});
