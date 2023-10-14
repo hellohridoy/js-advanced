0a0287731d14cd65ac7cccbae768e0255633ab95
Ridoy Rossain <ridoy.hossain@bracits.com>
2023-10-14 14:49:38 +0600

    Implementation of filter

--- Filter Function Implementation.js
+++ Filter Function Implementation.js
@@ -4,3 +4,18 @@ const newArray = arr.filter(function (value) {
   return value % 2 == 1;
 });
 console.log(newArray);
+//Implementation of Filter Fuction
+function myFilter(arr, callback) {
+  const array = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (callback(arr[i])) {
+      array.push(arr[i]);
+    }
+  }
+  return array;
+}
+
+let data = myFilter(arr, function (elements) {
+  return elements % 2 == 0;
+});
+console.log(data);
